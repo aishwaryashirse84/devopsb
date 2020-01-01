cc6e493fb80c4d747f2e17db2137ae4c0c9a6353
aishwaryashirse84 <adityashirse84@gmail.com>
2020-01-01 16:10:37 +0530

    initial commit message

---
+++ src/main/java/com/devopsb/DevopsbApplication.java
@@ -0,0 +1,13 @@
+package com.devopsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsbApplication.class, args);
+	}
+
+}
